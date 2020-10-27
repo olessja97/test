7f707c9edcdfe7615c873720871cd90773614c40
Olessja <olessja97@gmail.com>
2020-10-27 12:43:30 +0700

    New project commit.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CatsModule, ProductsModule],
 })
 export class AppModule {}
